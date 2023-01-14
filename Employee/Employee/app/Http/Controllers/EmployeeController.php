5267e22a41c861530336ffd56c4493831861216c
itsmaimai <21103807@usc.edu.ph>
2023-01-14 12:42:45 +0800

    Imported all files

---
+++ Employee/Employee/app/Http/Controllers/EmployeeController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Employee;
+class EmployeeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return Employee::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        return Employee::create([
+            'name'=>$request->name,
+            'address'=>$request->address,
+            'phoneNumber'=>$request->phoneNumber
+
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Employee::find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        return Employee::find($id)->update([
+            'name'=>$request->name,
+            'address'=>$request->address,
+            'phoneNumber'=>$request->phoneNumber
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return Employee::find($id)->delete();
+    }
+}
